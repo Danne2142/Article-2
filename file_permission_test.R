78255535b2e0feb48dd006e0186feb60762c318a
Daniel Wikström Shemer <daniel.wikstromshemer@gmail.com>
2025-02-10 22:09:06 +0100

    Remove file permission test logic and replace with a placeholder

--- file_permission_test.R
+++ file_permission_test.R
@@ -1,12 +1,1 @@
-file_path <- "C:/Users/danwik/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Data/EPICv1 and v2 data/All_metrics_crossectional_samples.csv"
-
-if (file.exists(file_path)) {
-  print("The file exists.")
-  if (file.access(file_path, 4) == 0) {
-    print("You have read permission for this file.")
-  } else {
-    stop("The file exists but you do not have read permission!")
-  }
-} else {
-  stop("The file does not exist!")
-}
+test
