fdc59e20d247c5b2fab0d8b74442ccddc538ce61
Danne2142 <daniel.wikstromshemer@gmail.com>
2025-03-19 15:22:41 +0100

    Update Metformin_analysis_Step9.R to change file paths and modify model runs for only healthy subjects

--- Metformin_analysis_Step9.R
+++ Metformin_analysis_Step9.R
@@ -1,21 +1,20 @@
 library(pacman)
 p_load(ggplot2, mice)
-source("C:/Users/danwik/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Program/modelingFunctions.R")
-source("C:/Users/danwik/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Program/plot_forest().R")
-source("C:/Users/danwik/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Program/forest_plot_fusion().R")
-source("C:/Users/danwik/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Program/run_models_metformin().R")
+source("C:/Users/danie/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Program/modelingFunctions.R")
+source("C:/Users/danie/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Program/plot_forest().R")
+source("C:/Users/danie/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Program/forest_plot_fusion().R")
+source("C:/Users/danie/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Program/run_models_metformin().R")
 
 
 
+
+#Only healthy
+
+#surv 1 
 # Load required data and set objects
-print("Generate main models")
-load("C:/Users/danwik/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Output/imputed_survey1")
-imp_data <- imputed_data
-df1_imp_data <- complete(imputed_data, 1) # only for visualizing imputed data
-# ...existing code to calculate missing summaries if needed...
+load("C:/Users/danie/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Output/imputed_survey1_only_healthy(diabeteswise)")
 
-
-run_models_metformin(
+results_surv1_only_healthy<-run_models_metformin(
 imp_data = imputed_data,
 # Define parameters 
 interventions = c("Metformin_new"),
@@ -24,6 +23,41 @@ lifestyle_covariates = c("Alcohol_per_week_numeric", "Education_levels_numeric",
                           "Main.Diet.Factor", "BMI", "Caffeine.Use_numeric", "Marital.Status_numeric", 
                           "Sexual.Frequency_numeric", "Hours.of.sleep.per.night_numeric"),
 covariates_to_always_include = c("Decimal.Chronological.Age", "Biological.Sex"),
-savePath = "C:/Users/danwik/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Output/",
-suffix = "_main_survey1"
+savePath = "C:/Users/danie/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Output/",
+suffix = "_only_healthy_survey1"
+)
+
+#surv 2
+# Load required data and set objects
+load("C:/Users/danie/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Output/imputed_survey2_only_healthy(diabeteswise)")
+
+results_surv2_only_healthy<-run_models_metformin(
+imp_data = imputed_data,
+# Define parameters 
+interventions = c("Metformin_new"),
+lifestyle_covariates = c("Alcohol_per_week_numeric",  
+  "Education_levels_numeric", "Stress.Level", "Tobacco.Use.Numeric", 
+  "sedentary_level","Red.Meat.times.per.week", "Processed.Food.times.per.week", "Feel.Well.Rested.days.per.week"),
+covariates_to_always_include = c("Decimal.Chronological.Age", "Biological.Sex"),
+savePath = "C:/Users/danie/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Output/",
+suffix = "_only_healthy_survey2"
+)
+
+
+#surv 3
+# Load required data and set objects
+load("C:/Users/danie/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Output/imputed_survey3_only_healthy(diabeteswise)")
+
+
+results_surv3_only_healthy<-run_models_metformin(
+imp_data = imputed_data,
+# Define parameters 
+interventions = c("Metformin_new"),
+lifestyle_covariates = c("Alcohol_per_week_numeric",  
+  "Education_levels_numeric", "Stress.Level", "Tobacco.Use.Numeric", "BMI", 
+  "sedentary_level","Red.Meat.times.per.week", "Processed.Food.times.per.week", 
+  "Feel.Well.Rested.days.per.week", "Hours.of.sleep.per.night_numeric"),
+covariates_to_always_include = c("Decimal.Chronological.Age", "Biological.Sex"),
+savePath = "C:/Users/danie/OneDrive - Karolinska Institutet/Documents/Project 2 - Vscode/Output/",
+suffix = "_only_healthy_survey3"
 )
